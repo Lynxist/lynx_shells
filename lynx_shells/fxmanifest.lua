15af4dfabb7350d5c41883b6f78edd171aab1b41
Lynxist <jimbobish@gmail.com>
2024-05-19 11:25:02 +0100

    lynx_shells - Upload

---
+++ lynx_shells/fxmanifest.lua
@@ -0,0 +1,15 @@
+lua54 'yes'
+
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+      'stream/ytyps/*.ytyp',
+      -- 'client.lua',
+}
+
+-- client_script 'client.lua'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyps/*.ytyp'
